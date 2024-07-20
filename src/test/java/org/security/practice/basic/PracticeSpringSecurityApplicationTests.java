888c78ea59b1e206ef1e765fc654e68c74c89f7d
Dani Imdadur <dir131203@gmail.com>
2024-07-20 17:05:00 +0700

    first commit

---
+++ src/test/java/org/security/practice/basic/PracticeSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.security.practice.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
