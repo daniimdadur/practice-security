234ebee6d7cddaa689b60d3e631f0835ffea3c6e
Dani Imdadur <dir131203@gmail.com>
2024-07-23 09:08:53 +0700

    feat: add dashboard index n login

---
+++ src/main/java/org/security/practice/basic/dashboard/HomeController.java
@@ -0,0 +1,20 @@
+package org.security.practice.basic.dashboard;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping("home")
+    public ModelAndView home() {
+        return new ModelAndView("pages/home/index");
+    }
+
+    @GetMapping("login")
+    public ModelAndView login() {
+        return new ModelAndView("pages/auth/login");
+    }
+}
